593988174f8424301429a8dbde72b06246bc66ed
f31ix_x <dadsontcg@gmail.com>
2026-09-06 00:16:13 -0600

    feat(jarvis): protect application behind OIDC auth gate

--- apps/jarvis-sec/src/main.tsx
+++ apps/jarvis-sec/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import AuthGate from './AuthGate';
 import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><App /></StrictMode>,
+  <StrictMode><AuthGate><App /></AuthGate></StrictMode>,
 );
